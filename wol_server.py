7cff8a8cae09bca48756b9f2101a608e6742af58
jlucas95 <jan-lucas@hetnet.nl>
2018-03-09 00:21:46 +0100

    fix for login_required url

--- wol_server.py
+++ wol_server.py
@@ -18,7 +18,7 @@ app.secret_key = os.urandom(32)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "/login"
+login_manager.login_view = url_for("login")
 
 
 @app.route("/")
